454b1bf441ae65ffd92c6fa6fb638cb2c995d8d7
Kody Brown <thewizard@wasatchwizard.com>
2017-01-09 15:53:33 -0700

    Formatted code

--- DelimitedFileReader.cs
+++ DelimitedFileReader.cs
@@ -1,26 +1,26 @@
-//
-// Copyright (C) 2005-2007 Kody Brown (kody@bricksoft.com).
-//
-// MIT License:
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to
-// deal in the Software without restriction, including without limitation the
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// sell copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
-//
+/*!
+	Copyright (C) 2005-2007 Kody Brown (kody@bricksoft.com).
+
+	MIT License:
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to
+	deal in the Software without restriction, including without limitation the
+	rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+	sell copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+	FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+	DEALINGS IN THE SOFTWARE.
+*/
 
 using System;
 using System.Collections.Generic;
@@ -74,10 +74,8 @@ namespace Bricksoft.PowerCode
 		/// <summary>
 		/// Gets whether the end of the file has been reached.
 		/// </summary>
-		public bool EndOfStream
-		{
-			get
-			{
+		public bool EndOfStream {
+			get {
 				if (Reader == null) {
 					return true;
 				}
@@ -303,17 +301,14 @@ namespace Bricksoft.PowerCode
 		/// <summary>
 		/// Gets or sets the file types.
 		/// </summary>
-		public static Dictionary<Guid, FileType> FileTypes
-		{
-			get
-			{
+		public static Dictionary<Guid, FileType> FileTypes {
+			get {
 				if (null == ms_fileTypes) {
 					ms_fileTypes = new Dictionary<Guid, FileType>();
 				}
 				return ms_fileTypes;
 			}
-			set
-			{
+			set {
 				ms_fileTypes = value;
 				if (null == ms_fileTypes) {
 					ms_fileTypes = new Dictionary<Guid, FileType>();
